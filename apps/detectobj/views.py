290e8e68e3280021dd3fc64e3a300ca8810cd77e
skanehfud279 <skanehfud279@gmail.com>
2024-05-17 03:58:55 +0900

    add image_detect_page add

--- apps/detectobj/views.py
+++ apps/detectobj/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 
@@ -15,6 +16,13 @@ import collections
 import io
 import os
 import tensorflow as tf
+
+from flask import render_template
+from tensorflow.keras.applications import VGG16 
+from tensorflow.keras.layers import Input
+import glob
+import json
+import yolov5
 
 # Create your views here.
 class InferencedImageDetectionView(LoginRequiredMixin, DetailView):
@@ -51,88 +59,97 @@ class InferencedImageDetectionView(LoginRequiredMixin, DetailView):
         )
         context["page_obj"] = page_obj
 
-    def post(self, request, *args, **kwargs):
-        img_qs = self.get_object()
-        img_bytes = img_qs.image.read()
-        img = I.open(io.BytesIO(img_bytes))
-
-        # Get form data
-        modelconf = self.request.POST.get("confidence")
-        modelconf = float(modelconf) if modelconf else settings.MODEL_CONFIDENCE
-        custom_model_id = self.request.POST.get("custom_model")
-        yolo_model_name = self.request.POST.get("yolo_model")
-
-        # Yolov5 dirs
-        yolo_weightsdir = settings.YOLOV5_WEIGTHS_DIR
-
-        # Whether user selected a custom model for the detection task
-        # An offline model will be used for detection provided user has
-        # uploaded this model.
-        if custom_model_id:
-            detection_model = MLModel.objects.get(id=custom_model_id)
-            model = tf.keras.models.load_model(detection_model.pth_filepath)
-            # print(model.names)
-
-        # Whether user selected a yolo model for the detection task
-        # Selected yolov5 model will be downloaded, and ready for object
-        # detection task. Yolov5 Api's will start working.
-        elif yolo_model_name:
-            model = tf.keras.models.load_model(os.path.join(yolo_weightsdir, yolo_model_name))
-
-        model.conf = modelconf
-        # classnames = model.names  (display classes in the model)
-
-        results = model(img, size=640)
-        results_list = results.pandas().xyxy[0].to_json(orient="records")
-        results_list = literal_eval(results_list)
-        classes_list = [item["name"] for item in results_list]
-        results_counter = collections.Counter(classes_list)
-        # if results_list == []:
-        #     messages.warning(
-        #         request, f'Model "{detection_model.name}" unable to predict. Try with another model.')
-        # else:
-        results.render()
-        media_folder = settings.MEDIA_ROOT
-        inferenced_img_dir = os.path.join(
-            media_folder, "inferenced_image")
-        if not os.path.exists(inferenced_img_dir):
-            os.makedirs(inferenced_img_dir)
-
-        # print(dir(results))
-        for img in results.ims:
-            img_base64 = I.fromarray(img)
-            img_base64.save(
-                f"{inferenced_img_dir}/{img_qs}", format="JPEG")
-
-        # Create/update the inferencedImage instance
-        inf_img_qs, created = InferencedImage.objects.get_or_create(
-            orig_image=img_qs,
-            inf_image_path=f"{settings.MEDIA_URL}inferenced_image/{img_qs.name}",
+    
+    # 폴더안의 파일 삭제 함수
+    def DeleteAllFiles(self, filePath):
+        if os.path.exists(filePath):
+            for file in os.scandir(filePath):
+                os.remove(file.path)
+            
+    def get_pagination(self, context, images_qs):
+        paginator = Paginator(
+            images_qs, settings.PAGINATE_DETECTION_IMAGES_NUM)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context["is_paginated"] = (
+            images_qs.count() > settings.PAGINATE_DETECTION_IMAGES_NUM
         )
-        inf_img_qs.detection_info = results_list
-        inf_img_qs.model_conf = modelconf
-        if custom_model_id:
-            inf_img_qs.custom_model = detection_model
-        elif yolo_model_name:
-            inf_img_qs.yolo_model = yolo_model_name
-        inf_img_qs.save()
-        tf.reset_default_graph()
+        context["page_obj"] = page_obj
 
-        # set image is_inferenced to true
-        img_qs.is_inferenced = True
-        img_qs.save()
-        # Ready for rendering next image on same html page.
-        imgset = img_qs.image_set
-        images_qs = imgset.images.all()
+    def post(self, request, *args, **kwargs):
+        self.DeleteAllFiles('C:/Users/admin/project3/project/project3/project3/static/aft')  #파일 업로드 후 새로 검사 시작할 때마다 폴더 내 파일 삭제
+        self.DeleteAllFiles('C:/Users/admin/project3/project/project3/project3/static/bef')  # 경로설정필요
+        
+        # 다중파일 업로드
+        if "file" not in request.FILES:
+            return HttpResponseRedirect(request.get_full_path())
+        file = request.FILES.getlist("file")
+        if not file:
+            return HttpResponse("Retry plz")
+        
+        model = VGG16(weight='imagenet')
+        resultlist=[]
+        pf=[]
+        for file in file:
+            filename = file.filename.rsplit("/")[0]     #파일경로에서 파일명만 추출
+            print("진행 중 파일 :", filename)
+
+            img_bytes = file.read()
+            img = I.open(io.BytesIO(img_bytes))
+            # print(img)
+            img.convert("RGB").save(f"static/bef/{filename}", format="PNG")
+            print('원본 저장')
+
+            results = model(img, size=640)
+            results_list = results.pandas().xyxy[0].to_json(orient="records")
+            results_list = literal_eval(results_list)
+            classes_list = [item["name"] for item in results_list]
+            result_counter = collections.Counter(classes_list)
+            
+            results.render()  # results.imgs에 바운딩박스와 라벨 처리
+
+            for img in results.ims:
+                img_base64 = I.fromarray(img)
+                img_base64.convert("RGB").save(f"static/aft/{filename}", format="PNG")
+                print('디텍트 저장')
+
+            resultlist.append(json.dumps(dict(result_counter)))
+
+            data = results.pandas().xyxy[0][['name']].values.tolist()   # results.imgs의 name값만 가져오기
+            print("데이터:",data)
+
+            if len(data) == 0:
+                pf.append("PASS")    # data 리스트의 값이 0이면 양품으로 pass
+            if len(data) != 0:
+                pf.append("FAIL")    # data 리스트의 값이 0이 아니면 불량으로 fail
+            
+            root = "static/aft"
+            if not os.path.isdir(root):      #파일명 리스트로 저장
+                return "Error : not found!"
+            files = []
+            for file in glob.glob("{}/*.*".format(root)):
+                fname = file.split(os.sep)[-1]
+                files.append(fname)
+            print("파일스 :",files)
+            
+            if len(files)>0:
+                firstimage = "static/aft/"+files[0]
+            else: pass
+
+            datanum = len(pf)
+            rate = round(pf.count('PASS') / len(pf), 3)
+            correct = pf.count('PASS')
+            
+            print(resultlist)
 
         # For pagination POST request
         context = {}
-        self.get_pagination(context, images_qs)
+        
 
-        context["img_qs"] = img_qs
-        context["inferenced_img_dir"] = f"{settings.MEDIA_URL}inferenced_image/{img_qs}"
+        
+        
         context["results_list"] = results_list
-        context["results_counter"] = results_counter
+        
         context["form1"] = AIModelForm()
         context["form2"] = InferencedImageForm()
         return render(self.request, self.template_name, context)
